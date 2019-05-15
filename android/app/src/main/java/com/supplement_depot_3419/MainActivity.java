27f90a43968b1421f1af2ca093de68a92005b0d0
Crowdbotics <team@crowdbotics.com>
2019-05-15 23:11:30 +0000

    initial commit

---
+++ android/app/src/main/java/com/supplement_depot_3419/MainActivity.java
@@ -0,0 +1,15 @@
+package com.supplement_depot_3419;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "supplement_depot_3419";
+    }
+}
